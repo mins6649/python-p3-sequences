135d9955a86f1c83a8b4ec414bfe5a5ca894fc1e
minyoungshin <mins6649@gmail.com>
2023-03-14 14:57:24 -0400

    fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fibonacci_list = [0, 1]
+        for i in range(length-2):
+            fibonacci_list.append(fibonacci_list[i] + fibonacci_list[i+1])
+        print(fibonacci_list)
+
+
+    
